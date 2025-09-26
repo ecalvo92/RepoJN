98f0de4f97cad37e7b1b846b74b6492601767455
Eduardo Calvo Castillo <edcalvo92@hotmail.com>
2025-09-25 19:39:40 -0600

    Receso

--- JN_ProyectoWeb/Models/UsuarioModel.cs
+++ JN_ProyectoWeb/Models/UsuarioModel.cs
@@ -2,5 +2,9 @@
 {
     public class UsuarioModel
     {
+        public string Identificacion { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+        public string CorreoElectronico { get; set; } = string.Empty;
+        public string Contrasenna { get; set; } = string.Empty;
     }
 }
