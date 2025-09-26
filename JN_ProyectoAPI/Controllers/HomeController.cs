2c5e5843f0c0d9074c0379b4cd4a9628dacf404b
Eduardo Calvo Castillo <edcalvo92@hotmail.com>
2025-09-25 20:49:53 -0600

    Fin de semana 3

---
+++ JN_ProyectoAPI/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using JN_ProyectoAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JN_ProyectoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("IniciarSesion")]
+        public IActionResult IniciarSesion(UsuarioModel usuario)
+        {
+            return Ok("HOLA");
+        }
+
+    }
+}
---
+++ JN_ProyectoAPI/Models/UsuarioModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JN_ProyectoAPI.Models
+{
+    public class UsuarioModel
+    {
+        [Required]
+        public string Identificacion { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+        public string CorreoElectronico { get; set; } = string.Empty;
+        public string Contrasenna { get; set; } = string.Empty;
+    }
+}
