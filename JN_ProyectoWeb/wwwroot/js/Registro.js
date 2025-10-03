cceb58fee3fc9eeb8f8b6c92d375e0972ae6a17d
Eduardo Calvo Castillo <edcalvo92@hotmail.com>
2025-10-02 20:21:18 -0600

    Consumo de api en js y validaciones

---
+++ JN_ProyectoWeb/wwwroot/js/Registro.js
@@ -0,0 +1,54 @@
+﻿function ConsultarNombre() {
+
+    let identificacion = $("#Identificacion").val();
+    $("#Nombre").val("");
+
+    if (identificacion.length >= 9) {
+
+        $.ajax({
+            type: 'GET',
+            url: 'https://apis.gometa.org/cedulas/' + identificacion,
+            dataType: 'json',
+            success: function (data) {
+                if (data.resultcount > 0)
+                { 
+                    $("#Nombre").val(data.nombre);
+                }
+            }
+        });
+
+    }
+}
+
+$(function () {
+    $("#FormRegistro").validate({
+        rules: {
+            Identificacion: {
+                required: true
+            },
+            Nombre: {
+                required: true
+            },
+            CorreoElectronico: {
+                required: true
+            },
+            Contrasenna: {
+                required: true
+            },
+        },
+        messages: {
+            Identificacion: {
+                required: "* Requerido"
+            },
+            Nombre: {
+                required: "* Requerido"
+            },
+            CorreoElectronico: {
+                required: "* Requerido"
+            },
+            Contrasenna: {
+                required: "* Requerido"
+            }
+        }
+    });
+});
