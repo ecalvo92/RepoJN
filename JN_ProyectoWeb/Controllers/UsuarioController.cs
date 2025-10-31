045d5bddc66cabd672ed9157f4d9a20258bfec90
Eduardo Calvo Castillo <edcalvo92@hotmail.com>
2025-10-30 19:23:19 -0600

    Diseño del formulario de Perfil

---
+++ JN_ProyectoWeb/Controllers/UsuarioController.cs
@@ -0,0 +1,15 @@
+﻿using JN_ProyectoWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JN_ProyectoWeb.Controllers
+{
+    [Seguridad]
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        public IActionResult Perfil()
+        {
+            return View();
+        }
+    }
+}
