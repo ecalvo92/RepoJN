c15664c599f1542c31dddf288bf8edfd7cff1200
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-05-28 18:28:45 -0600

    Creación de vistas solicitadas

--- JN_WEB/Controllers/HomeController.cs
+++ JN_WEB/Controllers/HomeController.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace JN_WEB.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        public IActionResult RecuperarAcceso()
         {
             return View();
         }
