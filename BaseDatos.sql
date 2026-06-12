e86f6f352088e3d22b8e59cee23f5797a31b0a99
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-06-11 20:51:54 -0600

    Update BaseDatos.sql

--- BaseDatos.sql
+++ BaseDatos.sql
@@ -7,6 +7,19 @@ GO
 USE [JN_BD]
 GO
 
+CREATE TABLE [dbo].[tbError](
+	[Consecutivo] [int] IDENTITY(1,1) NOT NULL,
+	[Mensaje] [varchar](max) NOT NULL,
+	[Lugar] [varchar](50) NOT NULL,
+	[FechaHora] [datetime] NOT NULL,
+	[ConsecutivoUsuario] [int] NOT NULL,
+ CONSTRAINT [PK_tbError] PRIMARY KEY CLUSTERED 
+(
+	[Consecutivo] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
 CREATE TABLE [dbo].[tbUsuario](
 	[Consecutivo] [int] IDENTITY(1,1) NOT NULL,
 	[Identificacion] [varchar](15) NOT NULL,
@@ -26,6 +39,35 @@ GO
 INSERT [dbo].[tbUsuario] ([Consecutivo], [Identificacion], [Nombre], [CorreoElectronico], [Contrasenna], [Estado]) VALUES (1, N'304590415', N'EDUARDO JOSE CALVO CASTILLO', N'ecalvo90415@ufide.ac.cr', N'90415', 1)
 GO
 SET IDENTITY_INSERT [dbo].[tbUsuario] OFF
+GO
+
+CREATE PROCEDURE [dbo].[spIniciarSesionUsuario]
+    @CorreoElectronico  varchar(100),
+    @Contrasenna        varchar(100)
+AS
+BEGIN
+
+    SELECT  Consecutivo,Identificacion,Nombre,CorreoElectronico,Estado
+    FROM    dbo.tbUsuario
+    WHERE   CorreoElectronico = @CorreoElectronico
+        AND Contrasenna = @Contrasenna
+        AND Estado = 1
+
+END
+GO
+
+CREATE PROCEDURE [dbo].[spRegistrarError]
+    @Mensaje            varchar(max),
+    @Lugar              varchar(50),
+    @FechaHora          datetime,
+    @ConsecutivoUsuario int
+AS
+BEGIN
+
+    INSERT INTO dbo.tbError(Mensaje,Lugar,FechaHora,ConsecutivoUsuario)
+    VALUES (@Mensaje,@Lugar,@FechaHora,@ConsecutivoUsuario)
+
+END
 GO
 
 CREATE PROCEDURE [dbo].[spRegistrarUsuario]
