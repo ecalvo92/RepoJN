401f716b69d5a346f262d0672c9da77171b30e1b
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-06-11 19:10:19 -0600

    Middleware de excepciones en el web

---
+++ JN_WEB/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JN_WEB.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult CapturarError()
+        {
+            var ex = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            return View("Error");
+        }
+    }
+}
--- JN_WEB/Program.cs
+++ JN_WEB/Program.cs
@@ -7,11 +7,10 @@ builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
+//Middleware de Errores
+app.UseExceptionHandler("/Error/CapturarError");
+
+app.UseHsts();
 
 app.UseHttpsRedirection();
 
