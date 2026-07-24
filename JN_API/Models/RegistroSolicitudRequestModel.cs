eeeb8483a2edb0e3be176f7007afb6225fdc48ac
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-07-23 19:31:26 -0600

    Registro de solicitudes

---
+++ JN_API/Models/RegistroSolicitudRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JN_API.Models
+{
+    public class RegistroSolicitudRequestModel
+    {
+        public string Titulo { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public int ConsecutivoAdmin { get; set; }
+    }
+}
