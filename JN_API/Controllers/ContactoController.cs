ebe6e204778157cda17e87d484eb5bdc5d03429b
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-08-06 19:01:08 -0600

    Cambios de Semana 13

---
+++ JN_API/Controllers/ContactoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JN_API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactoController : ControllerBase
+    {
+
+    }
+}
---
+++ JN_WEB/Controllers/ContactoController.cs
@@ -0,0 +1,14 @@
+﻿using JN_WEB.Filter;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JN_WEB.Controllers
+{
+    [SesionActivaAttribute]
+    public class ContactoController : Controller
+    {
+        public IActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
